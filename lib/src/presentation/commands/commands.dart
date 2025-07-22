c5163e9a5a1f9ccd2f9395469437f5941144bca2
Vinicius <viniciusmcruvinel@gmail.com>
2025-07-22 15:29:06 -0300

    refactor: rename delete command

--- lib/src/presentation/commands/commands.dart
+++ lib/src/presentation/commands/commands.dart
@@ -6,7 +6,7 @@ export 'analyze_command.dart';
 export 'add_owner_command.dart';
 export 'list_command.dart';
 export 'export_yaml_command.dart';
-export 'delete_command.dart';
+export 'delete_domain_command.dart';
 export 'config_command.dart';
 export 'clean_command.dart';
 export 'changelog_command.dart';
